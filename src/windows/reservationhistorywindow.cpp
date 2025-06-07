6978673e890d4a9b5b5a19e0853532a19031cc82
im6ha <mohamedb9177@gmail.com>
2025-06-07 14:02:25 +0100

    Update reservationhistorywindow.cpp

--- src/windows/reservationhistorywindow.cpp
+++ src/windows/reservationhistorywindow.cpp
@@ -11,7 +11,7 @@ reservationHistoryWindow::reservationHistoryWindow(QWidget *parent)
     ui->setupUi(this);
     setWindowTitle("Library Management System");
     QLabel *background = new QLabel(this);
-    QPixmap pixmap("../../media/background.png");
+    QPixmap pixmap(":/media/media/background.png");
     background->setPixmap(pixmap);
     background->setScaledContents(true);
     background->resize(this->size());
